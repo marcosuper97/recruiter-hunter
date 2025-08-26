0dcc831dc222534ce17b7f73ccdddb2f17a051ef
marcosuper97 <gorpishin97@yandex.ru>
2025-08-26 12:36:30 +0300

    Интерфейс поискового запроса

--- app/src/main/java/com/example/recruiterhunter/data/network/google_cse/GoogleCseApi.kt
+++ app/src/main/java/com/example/recruiterhunter/data/network/google_cse/GoogleCseApi.kt
@@ -1,5 +1,17 @@
 package com.example.recruiterhunter.data.network.google_cse
 
+import com.example.recruiterhunter.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface GoogleCseApi {
+    @GET("customsearch/v1")
+    suspend fun search(
+        @Query("key") apiKey: String = BuildConfig.GOOGLE_SEARCH_KEY,
+        @Query("cx") cx: String = BuildConfig.CUSTOM_SEARCH_ENGINE,
+        @Query("linkSite") linkSite: String,
+        @Query("start") start: Int,
+        @Query("q") query: String,
+    ): CseResponseDto
 }
