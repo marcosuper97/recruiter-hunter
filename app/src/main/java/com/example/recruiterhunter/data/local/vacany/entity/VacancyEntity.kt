0f7e0ff5d9e1bcb7a26b62044a3876748f1ebac0
marcosuper97 <gorpishin97@yandex.ru>
2025-09-09 17:25:33 +0300

    Clean code

--- app/src/main/java/com/example/recruiterhunter/data/local/vacany/entity/VacancyEntity.kt
+++ app/src/main/java/com/example/recruiterhunter/data/local/vacany/entity/VacancyEntity.kt
@@ -2,7 +2,6 @@ package com.example.recruiterhunter.data.local.vacany.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.Date
 
 @Entity(tableName = "vacancies")
 data class VacancyEntity(
