697ebe4cb546b43944581901bd02c32c30269cb6
marcosuper97 <gorpishin97@yandex.ru>
2025-09-11 23:22:28 +0300

    VacancySearchRepositoryImpl.kt [1]

--- app/src/main/java/com/example/recruiterhunter/data/impl/VacancySearchRepositoryImpl.kt
+++ app/src/main/java/com/example/recruiterhunter/data/impl/VacancySearchRepositoryImpl.kt
@@ -1,8 +1,13 @@
 package com.example.recruiterhunter.data.impl
 
+import com.example.recruiterhunter.data.converters.vacancy.preview.VacanciesPreviewConverter
+import com.example.recruiterhunter.data.network.vacancies.HhRetrofitClient
 import domain.repository.VacancySearchRepository
 
-class VacancySearchRepositoryImpl(): VacancySearchRepository {
+class VacancySearchRepositoryImpl(
+    private val hhRetrofitClient: HhRetrofitClient,
+    private val vacanciesPreviewConverter: VacanciesPreviewConverter
+) : VacancySearchRepository {
     override suspend fun doRequest(query: String, page: Int) {
         TODO("Not yet implemented")
     }
