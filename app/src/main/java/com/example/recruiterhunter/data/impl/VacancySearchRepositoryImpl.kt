02e379df3a65d4e45e0fe7188dcbe70239037caf
marcosuper97 <gorpishin97@yandex.ru>
2025-09-11 23:25:26 +0300

    VacancySearchRepositoryImpl.kt [2]

--- app/src/main/java/com/example/recruiterhunter/data/impl/VacancySearchRepositoryImpl.kt
+++ app/src/main/java/com/example/recruiterhunter/data/impl/VacancySearchRepositoryImpl.kt
@@ -2,13 +2,14 @@ package com.example.recruiterhunter.data.impl
 
 import com.example.recruiterhunter.data.converters.vacancy.preview.VacanciesPreviewConverter
 import com.example.recruiterhunter.data.network.vacancies.HhRetrofitClient
+import domain.model.vacancy.preview.VacancyPreview
 import domain.repository.VacancySearchRepository
 
 class VacancySearchRepositoryImpl(
     private val hhRetrofitClient: HhRetrofitClient,
     private val vacanciesPreviewConverter: VacanciesPreviewConverter
 ) : VacancySearchRepository {
-    override suspend fun doRequest(query: String, page: Int) {
-        TODO("Not yet implemented")
+    override suspend fun doRequest(query: String, page: Int): Result<VacancyPreview> = runCatching {
+        hhRetrofitClient.vacanciesSearchRequest()
     }
 }
--- app/src/main/java/domain/repository/VacancySearchRepository.kt
+++ app/src/main/java/domain/repository/VacancySearchRepository.kt
@@ -1,5 +1,7 @@
 package domain.repository
 
+import domain.model.vacancy.preview.VacancyPreview
+
 interface VacancySearchRepository {
-    suspend fun doRequest(query: String, page: Int = 1)
+    suspend fun doRequest(query: String, page: Int = 1): Result<VacancyPreview>
 }
