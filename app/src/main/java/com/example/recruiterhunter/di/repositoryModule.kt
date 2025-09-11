f2a40a9be42e9678ca3a092d2575f98c28a45899
marcosuper97 <gorpishin97@yandex.ru>
2025-09-11 23:23:34 +0300

    di repository

--- app/src/main/java/com/example/recruiterhunter/di/repositoryModule.kt
+++ app/src/main/java/com/example/recruiterhunter/di/repositoryModule.kt
@@ -1,7 +1,11 @@
 package com.example.recruiterhunter.di
 
+import com.example.recruiterhunter.data.impl.VacancySearchRepositoryImpl
+import domain.repository.VacancySearchRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
-
+    single<VacancySearchRepository> {
+        VacancySearchRepositoryImpl(get(), get())
+    }
 }
