2d4a90ea8caea83aae5c9f3d72f43db0df1bbaf3
marcosuper97 <gorpishin97@yandex.ru>
2025-09-15 21:22:57 +0300

    DI details vacancy

--- app/src/main/java/com/example/recruiterhunter/di/interactorModule.kt
+++ app/src/main/java/com/example/recruiterhunter/di/interactorModule.kt
@@ -1,5 +1,7 @@
 package com.example.recruiterhunter.di
 
+import com.example.recruiterhunter.domain.impl.VacancyDetailsInteractorImpl
+import com.example.recruiterhunter.domain.interactor.VacancyDetailsInteractor
 import domain.impl.VacancySearchInteractorImpl
 import domain.interactor.VacancySearchInteractor
 import org.koin.dsl.module
@@ -7,5 +9,9 @@ import org.koin.dsl.module
 val interactorModule = module {
     single<VacancySearchInteractor> {
         VacancySearchInteractorImpl(get())
+    }
+
+    single<VacancyDetailsInteractor> {
+        VacancyDetailsInteractorImpl(get())
     }
 }
--- app/src/main/java/com/example/recruiterhunter/di/repositoryModule.kt
+++ app/src/main/java/com/example/recruiterhunter/di/repositoryModule.kt
@@ -1,6 +1,8 @@
 package com.example.recruiterhunter.di
 
+import com.example.recruiterhunter.data.impl.VacancyDetailsRepositoryImpl
 import com.example.recruiterhunter.data.impl.VacancySearchRepositoryImpl
+import domain.repository.VacancyDetailsRepository
 import domain.repository.VacancySearchRepository
 import org.koin.dsl.module
 
@@ -8,6 +10,14 @@ val repositoryModule = module {
     single<VacancySearchRepository> {
         VacancySearchRepositoryImpl(
             get(),
+            get(),
+            get(),
+            get()
+        )
+    }
+
+    single<VacancyDetailsRepository> {
+        VacancyDetailsRepositoryImpl(
             get(),
             get(),
             get()
