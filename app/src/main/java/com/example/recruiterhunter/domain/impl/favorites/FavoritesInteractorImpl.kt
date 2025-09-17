b946b3e80a158a15265e847162ef6c198d44857b
marcosuper97 <gorpishin97@yandex.ru>
2025-09-17 12:22:49 +0300

    interactor init and impl

---
+++ app/src/main/java/com/example/recruiterhunter/domain/impl/favorites/FavoritesInteractorImpl.kt
@@ -0,0 +1,21 @@
+package com.example.recruiterhunter.domain.impl.favorites
+
+import com.example.recruiterhunter.domain.interactor.favorites.FavoritesInteractor
+import com.example.recruiterhunter.domain.repository.favorites.FavoritesRepository
+import domain.model.vacancy.details.Vacancy
+import kotlinx.coroutines.flow.Flow
+
+class FavoritesInteractorImpl(
+    private val favoritesRepository: FavoritesRepository
+) :
+    FavoritesInteractor {
+    override fun fetchFavorites(): Flow<List<Vacancy>> = favoritesRepository.fetchFavorites()
+
+    override suspend fun removeFromFavorites(id: Long) {
+        favoritesRepository.removeFromFavorites(id)
+    }
+
+    override suspend fun addToFavorites(vacancy: Vacancy) {
+        favoritesRepository.addToFavorites(vacancy)
+    }
+}
