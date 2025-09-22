d148b277af47593f24f3a2172091a2267d3d8574
marcosuper97 <gorpishin97@yandex.ru>
2025-09-22 21:53:45 +0300

    clean code

--- app/src/main/java/com/example/recruiterhunter/data/impl/theme_changer/ThemeChangerRepositoryImpl.kt
+++ app/src/main/java/com/example/recruiterhunter/data/impl/theme_changer/ThemeChangerRepositoryImpl.kt
@@ -5,7 +5,7 @@ import com.example.recruiterhunter.data.local.data_store.ThemeDataStore
 import com.example.recruiterhunter.domain.repository.theme_changer.ThemeChangerRepository
 import kotlinx.coroutines.flow.Flow
 
-class ThemeChangerRepositoryImpl(private val dataStore: ThemeDataStore): ThemeChangerRepository {
+class ThemeChangerRepositoryImpl(private val dataStore: ThemeDataStore) : ThemeChangerRepository {
     override fun getTheme(): Flow<ActualTheme> = dataStore.getTheme()
 
     override suspend fun changeTheme(theme: ActualTheme) {
--- app/src/main/java/com/example/recruiterhunter/data/local/data_store/ThemeDataStore.kt
+++ app/src/main/java/com/example/recruiterhunter/data/local/data_store/ThemeDataStore.kt
@@ -1,7 +1,5 @@
 package com.example.recruiterhunter.data.local.data_store
 
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.stringPreferencesKey
 import com.example.recruiterhunter.core.theme_changer.state.ActualTheme
 import kotlinx.coroutines.flow.Flow
 
