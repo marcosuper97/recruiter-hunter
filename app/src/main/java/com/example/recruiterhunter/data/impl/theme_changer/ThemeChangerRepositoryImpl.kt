742506745d0d31d7b12e03c08aba92a04fbb2bef
marcosuper97 <gorpishin97@yandex.ru>
2025-09-22 12:27:10 +0300

    repository impl init

---
+++ app/src/main/java/com/example/recruiterhunter/data/impl/theme_changer/ThemeChangerRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.example.recruiterhunter.data.impl.theme_changer
+
+import com.example.recruiterhunter.domain.repository.theme_changer.ThemeChangerRepository
+
+class ThemeChangerRepositoryImpl(): ThemeChangerRepository {
+    override suspend fun changeLight() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun changeDark() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun changeSystem() {
+        TODO("Not yet implemented")
+    }
+}
