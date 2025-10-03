ff99e888d43e2fc7bf7fc08ef9085836a090cf2c
marcosuper97 <gorpishin97@yandex.ru>
2025-10-03 13:05:47 +0300

    error state screen

--- app/src/main/java/com/example/recruiterhunter/ui/components/screen_states/NotFoundState.kt
+++ app/src/main/java/com/example/recruiterhunter/ui/components/screen_states/NotFoundState.kt
@@ -1,8 +1,77 @@
 package com.example.recruiterhunter.ui.components.screen_states
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.recruiterhunter.domain.model.theme_state.ActualTheme
+import com.example.recruiterhunter.ui.theme.RecruiterHunterTheme
 
 @Composable
-fun NotFoundScreen(){
+fun ErrorStateScreen(
+    modifier: Modifier = Modifier,
+    stateMessage: String? = null,
+    subMassageState: String? = null,
+    iconState: ImageVector? = null
+) {
+    val textStyle = MaterialTheme.typography
+    val colors = MaterialTheme.colorScheme
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            iconState?.let {
+                Icon(
+                    imageVector = it,
+                    tint = colors.primary,
+                    contentDescription = "",
+                    modifier = Modifier.size(125.dp)
+                )
+            }
+            stateMessage?.let {
+                Text(
+                    it,
+                    style = textStyle.displaySmall,
+                    textAlign = TextAlign.Center,
+                    color = colors.primary,
+                    fontWeight = FontWeight.Light
+                )
+            }
+            subMassageState?.let {
+                Text(
+                    it,
+                    style = textStyle.displaySmall,
+                    textAlign = TextAlign.Center,
+                    color = colors.primary,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.alpha(0.3f)
+                )
+            }
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun PreviewNotFoundScreen() {
+    RecruiterHunterTheme(ActualTheme.DARK) {
+        Scaffold { innerPaddings ->
+            NotFoundScreen(modifier = Modifier.padding(innerPaddings))
+        }
+    }
 }
