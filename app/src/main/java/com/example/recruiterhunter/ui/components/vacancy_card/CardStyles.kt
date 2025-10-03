cb1b1266c3747ba0069bd497117352fdd3187885
marcosuper97 <gorpishin97@yandex.ru>
2025-10-03 20:54:44 +0300

    clean code

--- app/src/main/java/com/example/recruiterhunter/ui/components/vacancy_card/CardStyles.kt
+++ app/src/main/java/com/example/recruiterhunter/ui/components/vacancy_card/CardStyles.kt
@@ -3,8 +3,7 @@ package com.example.recruiterhunter.ui.components.vacancy_card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+
 @Composable
 fun CorrectExample() {
     val colorScheme = MaterialTheme.colorScheme
