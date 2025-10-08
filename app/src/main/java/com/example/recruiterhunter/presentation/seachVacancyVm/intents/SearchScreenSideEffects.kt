43fae852cc48239f294cfbdb953b27152f35aecc
marcosuper97 <gorpishin97@yandex.ru>
2025-10-08 19:57:42 +0300

    Clean code

--- app/src/main/java/com/example/recruiterhunter/presentation/seachVacancyVm/intents/SearchScreenSideEffects.kt
+++ app/src/main/java/com/example/recruiterhunter/presentation/seachVacancyVm/intents/SearchScreenSideEffects.kt
@@ -4,6 +4,7 @@ sealed class SearchScreenSideEffects {
     data class OpenDetails(
         val route: String
     ) : SearchScreenSideEffects()
+
     data object OpenFilters : SearchScreenSideEffects()
     data object DownloadError : SearchScreenSideEffects()
 }
