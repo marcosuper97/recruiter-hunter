3e5a3f1b3cd28f1a80cdf2351ed5f0c52da19923
marcosuper97 <gorpishin97@yandex.ru>
2025-11-05 16:59:39 +0300

    очередная проработка

--- app/src/main/java/com/example/recruiterhunter/ui/components/details_top_bar/DetailsTopBar.kt
+++ app/src/main/java/com/example/recruiterhunter/ui/components/details_top_bar/DetailsTopBar.kt
@@ -155,7 +155,7 @@ fun DetailsTopBar(
                 Spacer(Modifier.padding(top = paddingTop))
                 TopBarButtons(
                     iconTint = elementsColor,
-                    onBackClick = {},
+                    onBackClick = {if (!isTransitionActive) navController.popBackStack()},
                     markBookClick = {},
                     shareClick = {}
                 )
